01a767b8fbae93f3e33c79494a25066910f821fe
Lucas Henrique Simon <lucassimon98@gmail.com>
2023-09-20 01:18:11 -0300

    Atualização de código com tentativas de subir aplicação para um container

--- src/test/java/com/example/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/example/springboot/SpringbootApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.springboot;
+//package com.example.springboot;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SpringbootApplicationTests {
+//@SpringBootTest
+//class SpringbootApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
-}
+//}
